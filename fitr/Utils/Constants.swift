112cc9772747da626ffeced029a95b16aa160d55
ryauy <ryanguyen1@gmail.com>
2025-03-30 04:48:39 -0400

    pretty much final commit! got all the functionality that should be needed

--- fitr/Utils/Constants.swift
+++ fitr/Utils/Constants.swift
@@ -20,13 +20,6 @@ struct APIKeys {
     static let openWeatherMapKey = "YOUR_API_KEY_HERE"
 }
 
-struct APIEndpoints {
-    static let baseURL = "YOUR_BACKEND_BASE_URL"
-    static let weatherEndpoint = "/weather"
-    static let outfitRecommendationEndpoint = "/outfit-recommendation"
-    static let clothingClassificationEndpoint = "/classify-clothing"
-}
-
 struct FirebaseCollections {
     static let users = "users"
     static let clothingItems = "clothingItems"
